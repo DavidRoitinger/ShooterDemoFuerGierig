93188c8fd40e67ae7014dd54374b374125d6fb14
KalichVivil <roitingerD190181@sus.htl-grieskirchen.at>
2024-06-04 11:22:49 +0200

    'GirigerDuStingatsKind'

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+    
+    TMP_Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <TMP_Text> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
